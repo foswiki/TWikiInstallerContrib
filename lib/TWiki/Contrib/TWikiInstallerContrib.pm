8f39c1ffc33a3ab2ebe64a5f8966ba162b1cde22
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-04 13:27:49 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule -- Sorry Micha, could not find a way to define specific values within the file
    git-svn-id: http://svn.foswiki.org/trunk/TWikiInstallerContrib@14686 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/TWiki/Contrib/TWikiInstallerContrib.pm
+++ lib/TWiki/Contrib/TWikiInstallerContrib.pm
@@ -20,15 +20,16 @@ use File::Basename qw( basename );
 use English;
 use Scalar::Util qw( tainted );
 use Data::Dumper qw( Dumper );
+
 #use Archive::Zip;
 ################################################################################
 
 # parameters
 # module: module filename relative to components (eg, kernels/Foswiki_trunk.zip or extension/BlogPlugin.zip)
 sub _InstallFoswikiExtension {
-    my ( $p ) = @_;
-    my $tmpInstall = $p->{tmpInstall} or die "tmpInstall";
-    my $module = $p->{module} or die "module";
+    my ($p) = @_;
+    my $tmpInstall     = $p->{tmpInstall}     or die "tmpInstall";
+    my $module         = $p->{module}         or die "module";
     my $mapFoswikiDirs = $p->{mapFoswikiDirs} or die "mapFoswikiDirs";
     my $localDirConfig = $p->{localDirConfig} or die "localDirConfig";
     my $perl = $p->{perl} || $EXECUTABLE_NAME;
@@ -45,60 +46,68 @@ sub _InstallFoswikiExtension {
 
     die "module tainted" if tainted $module;
 
-    my ( $name ) = ( basename $module ) =~ /(.*)\./;
+    my ($name) = ( basename $module ) =~ /(.*)\./;
     die "name is tainted" if tainted $name;
 
     print STDERR "FoswikiInstallerContrib: Installing $name\n";
     my $q = CGI->new() or die $!;
-    push @text, $q->b( $name );
+    push @text, $q->b($name);
 
-#    $ENV{TMPDIR} =~ /(.*)/;
-#    $ENV{TMPDIR} = $1;
+    #    $ENV{TMPDIR} =~ /(.*)/;
+    #    $ENV{TMPDIR} = $1;
 
-    my $archive = Archive::Zip::CommandLine->new( $module ) 
-	or warn qq{Archive::Zip::CommandLine new failed [$module] - can't install "$name"}, return 0;
+    my $archive = Archive::Zip::CommandLine->new($module)
+      or warn
+qq{Archive::Zip::CommandLine new failed [$module] - can't install "$name"},
+      return 0;
     $archive->extractTree( '', $INSTALL );
 
     foreach my $file ( $archive->memberNames ) {
-	# TODO: rename $base to something more descriptive (like ...?)
-	next unless my ($path,$base) = $file =~ m|^([^/]+)(/.*)$|;
 
-	my $map = $mapFoswikiDirs->{$path} or warn "no mapping for [$path]", next;
-	my $dirDest = $map->{dest} or die "no destination directory for [$path] " . Dumper( $map );
+        # TODO: rename $base to something more descriptive (like ...?)
+        next unless my ( $path, $base ) = $file =~ m|^([^/]+)(/.*)$|;
 
-	# handle directories (path ends with /?, if so, mirror directory structure)
-	mkpath( "$dirDest/$base" ), next if $base =~ m|/$|;
+        my $map = $mapFoswikiDirs->{$path}
+          or warn "no mapping for [$path]", next;
+        my $dirDest = $map->{dest}
+          or die "no destination directory for [$path] " . Dumper($map);
 
-	push @text, $file;
+     # handle directories (path ends with /?, if so, mirror directory structure)
+        mkpath("$dirDest/$base"), next if $base =~ m|/$|;
 
-	# install the file by moving it from the staging area
-	my $destFile = "$dirDest/$base";
-	mv( "$INSTALL/$file", $destFile ) or warn "$INSTALL/$file -> $destFile: $!";
-	chmod $map->{perms}, $destFile if $map->{perms};
+        push @text, $file;
 
-	# only Plugins have to be enabled (i.e., Contribs and Skins are "always on")
-	if ( my ( $plugin ) = $file =~ m|^lib/Foswiki/Plugins/(.+Plugin).pm$| ) {
-	    ++$plugins->{$plugin};
-	}
+        # install the file by moving it from the staging area
+        my $destFile = "$dirDest/$base";
+        mv( "$INSTALL/$file", $destFile )
+          or warn "$INSTALL/$file -> $destFile: $!";
+        chmod $map->{perms}, $destFile if $map->{perms};
 
-	# semi-KLUDGEy implementation to support ScriptSuffix
-	if ( $path eq 'bin' && $base !~ /\./ ) {		# process extension-less files
-	    my $origFile = $destFile;
-	    $destFile .= $localDirConfig->{ScriptSuffix};
-	    mv( $origFile, $destFile ) or die "$origFile -> $destFile: $!";
+    # only Plugins have to be enabled (i.e., Contribs and Skins are "always on")
+        if ( my ($plugin) = $file =~ m|^lib/Foswiki/Plugins/(.+Plugin).pm$| ) {
+            ++$plugins->{$plugin};
+        }
 
-	    # TODO: use an exception here!!!
-	    # patch perl path for local installation
-	    local $/ = undef;
-	    open( BIN, '<', $destFile ) or warn "unable to change perl path for $destFile: $!", next;
-	    my $bin = <BIN>;
-	    close BIN;
-	    $bin =~ s|/usr/bin/perl|$perl|;
+        # semi-KLUDGEy implementation to support ScriptSuffix
+        if ( $path eq 'bin' && $base !~ /\./ ) {  # process extension-less files
+            my $origFile = $destFile;
+            $destFile .= $localDirConfig->{ScriptSuffix};
+            mv( $origFile, $destFile ) or die "$origFile -> $destFile: $!";
 
-	    open( BIN, '>', $destFile ) or warn "unable to change perl path for $destFile: $!", next;
-	    print BIN $bin;
-	    close BIN;
-	}
+            # TODO: use an exception here!!!
+            # patch perl path for local installation
+            local $/ = undef;
+            open( BIN, '<', $destFile )
+              or warn "unable to change perl path for $destFile: $!", next;
+            my $bin = <BIN>;
+            close BIN;
+            $bin =~ s|/usr/bin/perl|$perl|;
+
+            open( BIN, '>', $destFile )
+              or warn "unable to change perl path for $destFile: $!", next;
+            print BIN $bin;
+            close BIN;
+        }
     }
 
     rmtree $INSTALL;
@@ -110,47 +119,38 @@ sub _InstallFoswikiExtension {
 ################################################################################
 package Archive::Zip::CommandLine;
 
-use constant AZ_OK           => 0;
-use constant AZ_ERROR        => 2;
+use constant AZ_OK    => 0;
+use constant AZ_ERROR => 2;
 
-sub new
-{
+sub new {
     my $class = shift;
-    my $self = bless( {
-	'fileName'                    => ''
-	},
-		      $class
-		      );
-#    $self->{'members'} = [];
-    if (@_)
-    {
-	my $status = $self->read(@_);
-	return $status == AZ_OK ? $self : undef;
+    my $self = bless( { 'fileName' => '' }, $class );
+
+    #    $self->{'members'} = [];
+    if (@_) {
+        my $status = $self->read(@_);
+        return $status == AZ_OK ? $self : undef;
     }
     return $self;
 }
 
-sub read
-{
+sub read {
     my ( $self, $filename ) = @_;
-    $self->{fileName} = Cwd::abs_path( $filename );
-    return
-	-e $filename
-	? AZ_OK
-	: AZ_ERROR;
+    $self->{fileName} = Cwd::abs_path($filename);
+    return -e $filename
+      ? AZ_OK
+      : AZ_ERROR;
 }
 
-sub extractTree
-{
+sub extractTree {
     my ( $self, undef, $tmpInstall ) = @_;
 
     $self->{extractedDir} = $tmpInstall;
     system( 'unzip', '-qq', $self->{fileName}, '-d' => $self->{extractedDir} );
 }
 
-sub memberNames
-{
-    my ( $self ) = @_;
+sub memberNames {
+    my ($self) = @_;
     chomp( my @a = grep { !/^\.$/ } `cd $self->{extractedDir}; find .` );
     @a = map { $_ .= '/' if -d "$self->{extractedDir}/$_"; $_ } @a;
     @a = map { s|^(\./)||; $_ } @a;
